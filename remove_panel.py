538a1eebc88b45112132bc02ad25093bee1bae78
rryo <rryowa@gmail.com>
2026-04-12 16:30:57 +0000

    grafana

--- remove_panel.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('grafana-dashboard.json', 'r') as f:
-    d = json.load(f)
-
-d['panels'] = [p for p in d['panels'] if p.get('title') != 'Candidate SNI Latency Trend (Direct)']
-
-with open('grafana-dashboard.json', 'w') as f:
-    json.dump(d, f, indent=2)
